a3bf7adef5cdd4bc9ceab8c0d4dd2d0ec9da905d
吴洋 <wuyang@tuhu.cn>
2023-05-10 13:56:11 +0800

    feature complete

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from app import socketio, app
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=10900)
+    socketio.run(app, host="0.0.0.0", port=10900, allow_unsafe_werkzeug=True)
